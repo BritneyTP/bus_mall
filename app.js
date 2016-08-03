e42cd4d9df6c60e906a9bcaccaa7a87296cb2b5a
Britney <britney.presley26@gmail.com>
2016-08-02 18:42:11 -0700

    tuesday chart completed

--- app.js
+++ app.js
@@ -140,3 +140,37 @@ var collectTallys = function() {
     tallyClicks.push(imageArray[i].tallysClicked);
   }
 };
+var data = {
+  labels: imageNames,
+  datasets: [
+    {
+      label: 'CHARTS',
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+      data: tallyClicks,
+    }
+  ]
+};
+
+function drawChart() {
+  var ctx = document.getElementById('canvas').getContext('2d');
+  var myBarChart = new Chart(ctx,{
+    type: 'bar',
+    data: data,
+  });
+};
